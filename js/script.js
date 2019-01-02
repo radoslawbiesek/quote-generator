9bf550230ab50c0e00ff005efdce848a19194eed
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-01-02 22:12:42 +0100

    Fix script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-(function() {
+// (function() {
     var prefix = "https://cors-anywhere.herokuapp.com/";
     var tweetLink = "https://twitter.com/intent/tweet?text=";
     var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
@@ -25,15 +25,15 @@
         }
 
         var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-    }
 
-    if (tweetText.length > 140) {
-        getQuote();
-    } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-        document.querySelector('.quote').innerText = quoteText;
-        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
-        document.querySelector('.tweet').setAttribute('href', tweet);
+        if (tweetText.length > 140) {
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            document.querySelector('.quote').innerText = quoteText;
+            document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+            document.querySelector('.tweet').setAttribute('href', tweet);
+        }
     }
 
     document.addEventListener('DOMContentLoaded', function() {
@@ -43,4 +43,4 @@
         });
     });
 
-})();
+// })();
